1998623f3d73af1929dc329d65dc5096f38c2916
Fredrick Ojure <fredojure@gmail.com>
2022-09-04 12:13:13 +0300

    4. Debbuged code to correct problem with sessions

--- chatserver.py
+++ chatserver.py
@@ -1,28 +1,43 @@
 from asyncore import dispatcher
-from asyn_chat import asyn_chat
-import socket, asyncore
+from asynchat import async_chat
+import socket
+import asyncore
 
 PORT = 5005
 NAME = 'TestChat'
 
-class ChatServer(dispatcher):
 
-    def __init__(self, port):
-        dispatcher.init (self)
+class ChatServer(dispatcher):
+    '''
+    A class that recieves connections and spawns individual sessions
+    It also handles broadcasts to the sessions
+    '''
+
+    def __init__(self, port, name):
+        dispatcher.__init__(self)
         self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
         self.set_reuse_addr()
         self.bind(('', port))
         self.listen(5)
+        self.name = name
+        self.sessions = []
 
+    def disconnect(self, session):
+        self.sessions.remove(session)
+
+    def broadcast(self, line):
+        for session in self.sessions:
+            session.push(line + '\r\n')
 
     def handle_accept(self):
         conn, addr = self.accept()
+        self.sessions.append(ChatSession(self, conn))
         print('Connection attempt from ', addr[0])
 
 
 if __name__ == '__main__':
-    s.ChatServer(PORT)
-        try:
-            asyncore.loop()
-        except KeyboardInterrupt:
-            print()
+    s = ChatServer(PORT, NAME)
+    try:
+        asyncore.loop()
+    except KeyboardInterrupt:
+        print()
--- chatsession.py
+++ chatsession.py
@@ -1,34 +1,37 @@
 from asyncore import dispatcher
-from asynchat import asyn_chat
-import socket, asyncore
+from asynchat import async_chat
+import socket
+import asyncore
 
 NAME = 'TestChat'
 
-class ChatSession(self, sock):
-	'''
-	A class that takes care of a connection between the server and a single user
-	'''
-	def __init__(self, sock):
-		#Standard setup tasks
-		asyn_chat.init(self, socket)
-		self.server = server
-		self.set_terminator('\r\n')
-		self.data = []
-		#Greet the user
-		self.push('Welcome to %s\r\n' %self.server.name)
 
-	def collect_incoming_data(self, dat):
-		self.data.append(data)
+class ChatSession(async_chat):
+    '''
+    A class that takes care of a connection between the server and a single user
+    '''
 
-	def found_terminator(self):
-		'''
-		If a terminator is found. that means a full line has
-		been read. Broadcast it to everyone
-		'''
-		line = ''.join(self.data)
-		self.data = []
-		self.server.broadcast(line)
+    def __init__(self, server, sock):
+        # Standard setup tasks
+        async_chat.__init__(self, socket)
+        self.server = server
+        self.set_terminator('\r\n')
+        self.data = []
+        # Greet the user
+        self.push('Welcome to %s\r\n' % self.server.name)
 
-	def handle_close(self):
-		asyn_chat.handle_close(self)
-		self.server.disconnect(self)
+    def collect_incoming_data(self, data):
+        self.data.append(data)
+
+    def found_terminator(self):
+        '''
+        If a terminator is found. that means a full line has
+        been read. Broadcast it to everyone
+        '''
+        line = ''.join(self.data)
+        self.data = []
+        self.server.broadcast(line)
+
+    def handle_close(self):
+        async_chat.handle_close(self)
+        self.server.disconnect(self)
